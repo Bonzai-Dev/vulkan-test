67634aeb6e5a42b08ce71ab3d1db2c4412843b1d
unknown <User@076-M22-A225810.ped.csmv.qc.ca>
2026-04-23 09:18:51 -0400

    Updated slang version

--- src/core/renderer/renderer.hpp
+++ src/core/renderer/renderer.hpp
@@ -2,8 +2,6 @@
 #include <memory>
 #include "render_context.hpp"
 
-// Wraps all the individual graphics APIs (Vulkan, DirectX, OpenGL, etc.) under a common interface.
-
 namespace Core::Graphics {
   enum class Backend {
     None,
