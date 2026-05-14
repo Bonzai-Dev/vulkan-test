ed5d25a87145b804993b7be549037fc9854cc650
unknown <User@076-M22-A225810.ped.csmv.qc.ca>
2026-05-14 17:11:08 -0400

    Swapchain

--- src/core/renderer/vulkan/vulkan_window.hpp
+++ src/core/renderer/vulkan/vulkan_window.hpp
@@ -1,10 +1,29 @@
 #pragma once
-
 #include "../window.hpp"
-#include "vulkan_swapchain.hpp"
+#include "vulkan_device.hpp"
 
 namespace Core::Graphics {
   class VulkanWindow: public Window {
+    enum SwapChainStatus {
+      /// We already called VulkanWindow::acquireNextImage.
+      ///
+      /// Can only go into this state if we're coming from SwapchainReleased
+      SwapChainAcquired,
+      /// We already called VulkanWindow::getImageAcquiredSemaphore.
+      /// Further calls to getImageAcquiredSemaphore will return null.
+      /// It is rendering or intends to into this swapchain.
+      ///
+      /// Can only go into this state if we're coming from SwapchainAcquired
+      SwapChainUsedInRendering,
+      /// We've come from SwapchainUsedInRendering and are waiting for
+      /// VulkanDevice::commitAndNextCommandBuffer to present us
+      SwapChainPendingSwap,
+      /// We don't own a swapchain. Cannot render to this window.
+      ///
+      /// This status should not last long unless we're not initialized yet.
+      SwapChainReleased
+    };
+    
     public:
       VulkanWindow(
        const VulkanDevice &device,
@@ -13,7 +32,43 @@ namespace Core::Graphics {
        const WindowOptions &windowOptions
       );
 
+      VulkanWindow(const VulkanWindow &other) = delete;
+
+      VulkanWindow &operator=(const VulkanWindow &other) = delete;
+
+      VulkanWindow(VulkanWindow &&other) = delete;
+
+      VulkanWindow &operator=(VulkanWindow &&other) = delete;
+
+      ~VulkanWindow();
+    
     private:
-      VulkanSwapChain swapChain;
+      VkImageView createImageView(
+        const VkImage &image,
+        const VkFormat &format,
+        VkImageAspectFlags aspectFlags,
+        VkImageViewType viewType,
+        std::uint32_t layerCount,
+        std::uint32_t mipLevels
+      ) const;
+
+      void destroyImageView(const VkImageView &imageView) const;
+
+      void createSurface();
+
+      void createSwapChain();
+
+      VkSurfaceFormatKHR chooseSurfaceFormat() const;
+
+      VkPresentModeKHR choosePresentMode() const;
+
+      const VkInstance &instance;
+      const VulkanDevice &device;
+
+      VkSurfaceKHR surface = VK_NULL_HANDLE;
+      VkSwapchainKHR swapChain = VK_NULL_HANDLE;
+
+      std::vector<VkImage> swapChainImages;
+      std::vector<VkImageView> swapChainImageViews;
   };
 }
